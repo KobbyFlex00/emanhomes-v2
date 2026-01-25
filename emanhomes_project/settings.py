5622d6a31a53caeb2686be56f12a0771308913ee
KobbyFlex00 <kobbyflex1516@gmail.com>
2026-01-25 01:32:11 +0000

    Switch to persistent database

--- emanhomes_project/settings.py
+++ emanhomes_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,6 +78,7 @@ WSGI_APPLICATION = 'emanhomes_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Database Configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,6 +86,10 @@ DATABASES = {
     }
 }
 
+# If running on Render, use the PostgreSQL database
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
