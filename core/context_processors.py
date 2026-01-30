9c3cc2ff2b358281269d9487e0f8572363abd5db
KobbyFlex00 <kobbyflex1516@gmail.com>
2026-01-30 03:24:20 +0000

    Changes to context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,8 +1,14 @@
 from .models import SiteConfiguration
 
 def site_configuration(request):
+    """
+    Retrieves the first SiteConfiguration object found in the database.
+    If none exists, it returns None (preventing a crash).
+    """
     try:
+        
         config = SiteConfiguration.objects.first()
-    except:
+    except Exception:
         config = None
+        
     return {'site_config': config}
